f11abdf37f46b8944376687aa7f7cbf9f4194278
Jan Cizmar <cizmar@chlupac.com>
2019-10-27 01:03:46 +0200

    Test ldap server

--- src/test/java/com/polygloat/JwtTest.java
+++ src/test/java/com/polygloat/JwtTest.java
@@ -1,29 +1,20 @@
 package com.polygloat;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.polygloat.configuration.WebSecurityConfig;
 import com.polygloat.security.payload.LoginRequest;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashMap;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,7 +28,7 @@ class JwtTest {
 
     @Test
     void generatesTokenForValidUser() throws Exception {
-        String response = doAuthentication("ben", "benaspassword")
+        String response = doAuthentication("ben", "benspassword")
                 .getResponse().getContentAsString();
 
         HashMap result = new ObjectMapper().readValue(response, HashMap.class);
